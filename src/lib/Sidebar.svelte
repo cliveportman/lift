71dd34b7cdce68b2979c3ec1b507fb789da7df57
cliveportman <clive@cliveportman.co.uk>
2022-07-24 13:38:16 +0100

    Basic sidebar layout

--- src/lib/Sidebar.svelte
+++ src/lib/Sidebar.svelte
@@ -1,5 +1,28 @@
+<div class="relative overflow-hidden py-10 px-6 bg-gradient-to-b from-teal-500 to-teal-600 sm:px-10 xl:p-12 text-left">
+  <h3 class="text-lg font-medium text-white">Add your journey</h3>
+  <p class="mt-6 text-base text-teal-50 max-w-3xl">Whether you're looking to Liftshare as a driver or a passenger, listing your journey is the best way to find a match.</p>
 
-<div class="relative overflow-hidden py-10 px-6 bg-gradient-to-b from-teal-500 to-teal-600 sm:px-10 xl:p-12">
-  <h3 class="text-lg font-medium text-white">Contact information</h3>
-  <p class="mt-6 text-base text-teal-50 max-w-3xl">Nullam risus blandit ac aliquam justo ipsum. Quam mauris volutpat massa dictumst amet. Sapien tortor lacus arcu.</p>
+  <div class="mt-6">
+    <label for="journey-start" class="block text-medium font-medium text-white">Journey start</label>
+    <div class="mt-1">
+      <input type="text" name="" id="journey-start" class="py-3 px-4 block w-full shadow-sm text-warm-gray-900 focus:ring-gray-500 focus:border-grey-500 border-warm-gray-300 rounded-md">
+    </div>
+  </div>
+
+  <div class="mt-6">
+    <label for="journey-destination" class="block text-medium font-medium text-white">Destination</label>
+    <div class="mt-1">
+      <input type="text" name="" id="journey-destination" class="py-3 px-4 block w-full shadow-sm text-warm-gray-900 focus:ring-gray-500 focus:border-grey-500 border-warm-gray-300 rounded-md">
+    </div>
+  </div>
+
+  <div class="mt-6 relative flex items-start">
+    <div class="flex items-center h-5">
+      <input id="comments" aria-describedby="comments-description" name="comments" type="checkbox" class="focus:ring-indigo-500 h-4 w-4 text-indigo-600 border-gray-300 rounded">
+    </div>
+    <div class="ml-3">
+      <label for="comments" class="text-medium font-medium text-white">This is a return journey (round trip)</label>
+    </div>
+  </div>
+
 </div>
