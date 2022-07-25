a3c7c0d6bf00e05a752f70fa8896cba3c18c5dff
cliveportman <clive@cliveportman.co.uk>
2022-07-25 12:51:39 +0100

    Disabling buttons until ready

--- src/lib/Content.svelte
+++ src/lib/Content.svelte
@@ -2,19 +2,21 @@
 
   import { mapsApiLoaded, locations, Utilities, journey, uiStage } from "./stores"
 
-  let mapContainer, map, ready, center,
+  let mapContainer, map, opacity, center,
   zoom = 8
 
   
 
   $: if (mapContainer && $mapsApiLoaded && $locations.start && $locations.destination) {
     center = { lat: $locations.start.geometry.location.lat(), lng: $locations.start.geometry.location.lng() }
-    ready = 1
+    opacity = 1
     createMap()    
   }
 
   $: if ($journey.route == null) {
-    ready = 0
+    // We're recommended to re-use maps rather than delete them. So, when clearing the user's chouces
+    // we just hide the existing map until it's ready to be used again
+    opacity = 0
   }
 
   let createMap = async () => {
@@ -26,12 +28,11 @@
       }
     )
     $journey.route = await Utilities.createRoute(map, $locations.start, $locations.destination)
-    $uiStage = 'schedule'
   }
 
 </script>
 <div class="col-span-2">
-  <div class="container" bind:this="{mapContainer}" style:opacity="{ready}"></div>
+  <div class="container" bind:this="{mapContainer}" style:opacity="{opacity}"></div>
 </div>
 
 <style lang="scss">
